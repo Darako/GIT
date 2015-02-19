2650eca79b8c6b0d7bb36b639bd7c6fbe444fd16
Jorge González <darako.genesis@gmail.com>
2015-02-19 17:26:45 +0100

    Creacion del repositorio

---
+++ Videojuegos/src/videojuegos/pec1/Espada.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package videojuegos.pec1;
+
+/**
+ *
+ * @author Jorge
+ */
+public class Espada extends Arma{
+    
+    //ATRIBUTOS
+    private int resistencia;
+
+    //CONSTRUCTOR
+    public Espada(String nombre, int danyo, int resistencia) {
+        super(nombre, danyo);
+        this.resistencia = resistencia;
+    }
+
+    //GETTER & SETTER
+    public int getResistencia() {
+        return resistencia;
+    }
+    public void setResistencia(int resistencia) {
+        this.resistencia = resistencia;
+    }
+
+    //METODOS
+    public void usar(){
+        this.resistencia--;
+    }
+    
+    @Override
+    public String toString() {
+        return "\nNombre: "+super.getNombre()+"\nDaño: "+super.getDanyo()+"\nResistencia: "+resistencia;
+    }
+
+    
+    
+}
---
+++ Videojuegos/src/videojuegos/pec1/JuegazoPapa.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package videojuegos.pec1;
+
+/**
+ *
+ * @author Jorge
+ */
+public class JuegazoPapa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Arco arco1 = new Arco("Tirachinas de Bart", 5, 10);
+        Espada espada1 = new Espada("Palo de Selfie", 5, 20);
+        Baston baston1 = new Baston("Palillo de los Oidos", 5, 15);
+        
+        Arma baul[] = new Arma[3];
+        
+        baul[0] = arco1;
+        baul[1] = espada1;
+        baul[2] = baston1;
+        
+        arco1.usar();
+        espada1.usar();
+        baston1.usar();
+        
+        
+        
+        for(int i=0;i<baul.length;i++)
+        {
+            System.out.println(baul[i].toString());
+        }
+       
+        
+       
+    }
+    
+}
