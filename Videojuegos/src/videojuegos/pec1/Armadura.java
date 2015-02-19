1a134ee39fa574b00288bb52b7b14c9bc4cfe57b
Jorge González <darako.genesis@gmail.com>
2015-02-19 18:33:17 +0100

    Más Armaduras

--- Videojuegos/src/videojuegos/pec1/Armadura.java
+++ Videojuegos/src/videojuegos/pec1/Armadura.java
@@ -11,4 +11,33 @@ package videojuegos.pec1;
  */
 public class Armadura {
     
+    //ATRIBUTOS
+    private int defensa;    
+    private int durabilidad;
+    
+    //CONSTRUCTOR
+    public Armadura(int defensa, int durabilidad) {
+        this.defensa = defensa;
+        this.durabilidad = durabilidad;
+    }
+
+    //GETTER & SETTER
+    public int getDurabilidad() {
+        return durabilidad;
+    }
+
+    public void setDurabilidad(int durabilidad) {
+        this.durabilidad = durabilidad;
+    }
+
+
+    public int getDefensa() {
+        return defensa;
+    }
+
+    public void setDefensa(int defensa) {
+        this.defensa = defensa;
+    }
+
+    
 }
